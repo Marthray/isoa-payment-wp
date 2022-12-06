8569dd7e43d753113f378163dbed293ad04dec34
Brian Amaya <brianjesusmoreno@gmail.com>
2022-12-05 21:42:06 -0400

    fix bvc

--- isoa-plugin-core.php
+++ isoa-plugin-core.php
@@ -325,6 +325,10 @@ function isoa_init_gateway_class() {
             wp_register_script( 'woocommerce_bank_isoa', plugins_url( 'assets/banks.js?v=2.1', __FILE__ ), array( 'jquery' ) );
             wp_enqueue_script( 'woocommerce_bank_isoa' );
 
+            wp_enqueue_script('sweetAlert2', 'https://cdn.jsdelivr.net/npm/sweetalert2@11');
+	        wp_register_script( 'woocommerce_bvc_isoa', plugins_url( 'assets/bvc.js?v=1.1', __FILE__ ), array( 'jquery' ) );
+            wp_enqueue_script( 'woocommerce_bvc_isoa' );
+
 	 	}
 
 		/*
